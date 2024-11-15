0d9b334bfd990fe00236ed4006b9532a0067979b
Didi Walakuttij <diwalaku@f0r1s14.codam.nl>
2024-11-15 14:52:57 +0100

    update

--- all-training/level3/biggestpal.h
+++
@@ -1,9 +0,0 @@
-#ifndef BIGGEST_PAL_H
-# define BIGGEST_PAL_H
-
-# include <unistd.h>
-
-int		is_palindrome(char *str, int start, int end);
-void	write_palindrome(char *str, int start, int end);
-
-#endif
